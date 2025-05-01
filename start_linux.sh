16633bd767058fa652125fae41e61c9358986e82
Liam Tormey <lltsamoa@gmail.com>
2025-05-01 17:04:39 +1000

    Create start_linux.sh

---
+++ start_linux.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+
+sudo apt update
+sudo apt-get install terraform
+
+cd ./terraform/nvidia
+terraform apply 
